310a86154b2966d1bdda6cfec47658d54216a707
Enrique Zamora <72171173+zamora1221@users.noreply.github.com>
2025-09-23 09:08:17 -0500

    Update wsgi.py

--- bailsaas/wsgi.py
+++ bailsaas/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bailsaas.settings")
 application = get_wsgi_application()
